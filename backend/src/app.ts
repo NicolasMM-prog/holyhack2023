6f857ea23670cb786f7598dc6e61486df22c3bc3
Rob Nickmans <rob.nickmans@rndevelopment.be>
2023-03-11 01:32:42 +0100

    init express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,24 @@
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors'
 
-export function start() {
-  console.log('Hello world!')
+export async function start(): Promise<void> {
+  const server: Express = express()
+  const app = express.Router()
+  
+  server.use('/api/', app)
+  
+  app.use(express.json())
+  app.use(
+    cors({
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+  )
+  
+  app.get('/', handleRoot)
+    
+  server.listen(3000, () => console.info('Backend running!'))
+}
+  
+function handleRoot(_req: Request, res: Response): void {
+  res.json('Running backend')
 }
