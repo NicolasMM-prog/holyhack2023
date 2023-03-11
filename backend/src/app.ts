dd773da680c078f94f2caf6e14dcac3e723a3d97
NicolasMM-prog <66412695+NicolasMM-prog@users.noreply.github.com>
2023-03-11 03:15:49 +0100

    colruyt scraper

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,32 @@
 import express, { Express, Request, Response } from 'express'
 import cors from 'cors'
+import { scrapeProducts } from './scraper.js'
 
 export async function start(): Promise<void> {
   const server: Express = express()
   const app = express.Router()
-  
-  server.use('/api/', app)
-  
+
+  server.use('/', app)
+
   app.use(express.json())
   app.use(
     cors({
       allowedHeaders: ['Content-Type', 'Authorization'],
     }),
   )
-  
+
   app.get('/', handleRoot)
-    
+  app.get('/search', handleSearch)
+
   server.listen(3000, () => console.info('Backend running!'))
 }
-  
+
 function handleRoot(_req: Request, res: Response): void {
   res.json('Running backend')
+}
+
+// www.rndevelopment.be/search?q=kaas
+async function handleSearch(req: Request, res: Response): Promise<void> {
+  const searchParam = req.query.q as string
+  res.json(await scrapeProducts(searchParam))
 }
---
+++ backend/src/scraper.ts
@@ -0,0 +1,75 @@
+import * as cheerio from 'cheerio'
+import got from 'got'
+
+export async function scrapeProducts(searchTerm: string): Promise<Product[]> {
+    return await scrapeProductsColruyt(searchTerm)
+}
+
+export async function scrapeProductsColruyt(searchTerm: string): Promise<Product[]> {
+  const selector = '#plpProducts > div.plp__items > div.plp-products > div'
+
+  const response = await got(`https://www.collectandgo.be/colruyt/nl/zoek?searchTerm=${searchTerm}`, {
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.63',
+    },
+  })
+  const $ = cheerio.load(response.body)
+
+  const products = [] as Product[]
+
+  for (let i = 1; i < 11; i++) {
+    const price =
+      normalize(
+        $(
+          `${selector} > div:nth-child(${i}) > div > div.plp-item__price > div.plp-item__price-display > div > span.c-price__euro`,
+        ).html(),
+      ) +
+      normalize(
+        $(
+          `${selector} > div:nth-child(${i}) > div > div.plp-item__price > div.plp-item__price-display > div > span.c-price__cent`,
+        ).html(),
+      )
+    products.push({
+      title: normalize(
+        $(
+          `${selector} > div:nth-child(${i}) > div > div.plp-item-top > div.c-tile__data > div > div.c-product-name__long.--plp`,
+        ).html(),
+      ),
+      brand: normalize(
+        $(
+          `${selector} > div:nth-child(${i}) > div > div.plp-item-top > div.c-tile__data > div > div.c-product-name__brand.--plp`,
+        ).html(),
+      ),
+      priceKilo: normalize(
+        $(
+          `${selector} > div:nth-child(${i}) > div > div.plp-item__price > div.plp-item__price-display > div > span.product__price__weight-price > span.product__price__volume-price.s-volume-price`,
+        ).html(),
+      ),
+      image: normalize(
+        $(
+          `${selector} > div:nth-child(${i}) > div > div.plp-item-top > div.plp-item-top__image-banner-wrap > a > img`,
+        ).attr('src'),
+      ),
+      price,
+    })
+  }
+
+  return products
+}
+
+export async function scrapeProductsAH(searchTerm: string): Promise<Product[]> {
+    
+}
+
+function normalize(data: string | null | undefined): string {
+  return (data || '').trim().replace(/&nbsp;/g, '')
+}
+
+type Product = {
+  title: string
+  price: string
+  brand: string
+  priceKilo: string
+  image: string
+}
